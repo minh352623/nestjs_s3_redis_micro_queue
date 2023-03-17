028e3b0bd813d471944c109b7b6a63bfa0ff94b7
minh352623 <congminh352623@gmail.com>
2023-03-17 21:52:39 +0700

    setup  auth

---
+++ src/user/services/auth.service.ts
@@ -0,0 +1,47 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto, LoginUserDto } from 'src/user/dto/user.dto';
+import { UserService } from 'src/user/services/user.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(userDto: CreateUserDto) {
+    const user = await this.userService.create(userDto);
+    const token = this._createToken(user);
+    return {
+      email: user.email,
+      ...token,
+    };
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const user = await this.userService.findByLogin(loginUserDto);
+    const token = this._createToken(user);
+
+    return {
+      email: user.email,
+      ...token,
+    };
+  }
+
+  async validateUser(email: string) {
+    const user = await this.userService.findByEmail(email);
+    if (!user) {
+      throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
+    }
+    return user;
+  }
+
+  private _createToken({ email }): any {
+    const accessToken = this.jwtService.sign({ email });
+    return {
+      expiresIn: process.env.EXPIRESIN,
+      accessToken,
+    };
+  }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,12 +3,25 @@ import { UserService } from './services/user.service';
 import { UserController } from './controllers/user.controller';
 import { User, UserSchema } from './schemas/user.schema';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import 'dotenv/config';
+import { UserRepository } from './repositories/user.repository';
+import { JwtStrategy } from './jwt.strategy';
+import { AuthService } from './services/auth.service';
+import { AuthController } from './controllers/auth.controller';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      secret: process.env.SERECT_JWT,
+      signOptions: {
+        expiresIn: process.env.EXPIRESIN,
+      },
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
   ],
-  providers: [UserService],
-  controllers: [UserController],
+  providers: [UserService, UserRepository, JwtStrategy, AuthService],
+  controllers: [UserController, AuthController],
 })
 export class UserModule {}
