d3bb62a6d0fee0ba4de333f8302fa0d74fd4b173
minh352623 <congminh352623@gmail.com>
2023-04-02 08:18:53 +0700

    setup rabbitMq and connect service

--- src/subscriber/subscriber.controller.ts
+++ src/subscriber/subscriber.controller.ts
@@ -1,13 +1,28 @@
-import { Controller, Get, Inject, Post, Req, UseGuards } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import {
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Req,
+  UseGuards,
+  OnModuleInit,
+} from '@nestjs/common';
+import { ClientGrpc, ClientProxy } from '@nestjs/microservices';
 import { AuthGuard } from '@nestjs/passport';
+import SubscriberInterface from './subscriber.interface';
 
 @Controller('subscriber')
 export class SubscriberController {
   constructor(
     @Inject('SUBSCRIBER_SERVICE')
-    private readonly subscriberService: ClientProxy,
-  ) {}
+    private readonly subscriberService: ClientProxy, // @Inject('SUBSCRIBER_SERVICE')
+  ) // private client: ClientGrpc,
+  {}
+  // private gRpcService;
+  // onModuleInit(): any {
+  //   this.gRpcService =
+  //     this.client.getService<SubscriberInterface>('SubscriberService');
+  // }
 
   @Get()
   @UseGuards(AuthGuard('jwt'))
@@ -43,4 +58,28 @@ export class SubscriberController {
     );
     return true;
   }
+
+  @Post('rmq')
+  @UseGuards(AuthGuard('jwt'))
+  async createPost(@Req() req: any) {
+    return this.subscriberService.send(
+      {
+        cmd: 'add-subscriber',
+      },
+      req.user,
+    );
+  }
+
+  //GRPC
+
+  // async getSubscribers() {
+  //   return this.gRpcService.getAllSubscribers({});
+  // }
+
+  // async createPost(@Req() req: any) {
+  //   return this.gRpcService.addSubscriber({
+  //     email: req.user.email,
+  //     name: req.user.name,
+  //   });
+  // }
 }
